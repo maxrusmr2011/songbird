09f585712a2922ef6f1b0e11d8d8cbb6f8ed0972
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-08-14 17:16:13 +0300

    feat: add Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import './Header.scss';
 
 export default class Header extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
+  
   render() {
     const { levels, numLevel, score } = this.props;
     return (
-      <header>
-        <h1 className="logo">Logo</h1>
-        <div className="score">Score: {score}</div>
-        <ul className="level">
-          { levels.map((item, i) => (<li className={`level__item${numLevel === i ? ' level__item-active' : ''}` }>{item} {i}</li>)) }
-        </ul>
+      <header className="header">
+        <div className="wrapper">
+          <h1 className="header__logo">Song<span>bird</span></h1>
+          <div className="header__score">Score: {score}</div>
+          <ul className="level">
+            { levels.map((item, i) => (
+              <li 
+                key={i} 
+                className={`level__item${numLevel === i ? ' level__item-active' : ''}`}>
+                  {item}
+              </li>
+            )) }
+          </ul>
+        </div>
       </header>
     );
   }
